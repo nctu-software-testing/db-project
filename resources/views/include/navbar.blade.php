a5c299cd970338fe36d0f0015f473f6410280a5d
WeiChun <weichun@csie.tw>
2017-11-28 00:44:35 +0800

    修改上方選單UI

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -3,54 +3,33 @@
 @else
     @include('include.navbar-login')
 @endif
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
-    <a class="navbar-brand" href="{{asset('/')}}">
-        <img src="{{asset('images/favicon.png')}}" id="logo-img"/>
-        Any Buy
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse"
-            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        <!--
-            <li class="nav-item active">
-                <a class="nav-link waves-effect waves-light" href="{{asset('/')}}">Home <span class="sr-only">(current)</span></a>
-            </li>
-            -->
-            <li class="nav-item">
-                <a class="nav-link waves-effect waves-light" accesskey="f"
-                   href="{{action('ProductController@getProduct')}}">商品瀏覽</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle waves-effect waves-light" id="navAdminBtn" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false">
-                    管理
-                </a>
-                <div class="dropdown-menu dropdown-primary" aria-labelledby="navAdminBtn">
-                    <a class="dropdown-item waves-effect waves-light" accesskey="b"
-                       href="{{action('VerificationController@getVerification')}}">會員驗證</a>
-
-                    <a class="dropdown-item waves-effect waves-light" accesskey="e"
-                       href="{{action('CategoryController@getCategory')}}">類別管理</a>
-
-                    <a class="dropdown-item waves-effect waves-light" accesskey="g"
-                       href="{{action('DiscountController@getDiscount')}}">折價管理</a>
-
-                    <a class="dropdown-item waves-effect waves-light" accesskey="h"
-                       href="{{action('OrderController@getOrder')}}">訂單管理</a>
-
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar" id="headNav">
+    <div class="container">
+        <a class="navbar-brand" href="{{asset('/')}}">
+            <img src="{{asset('images/favicon.png')}}" id="logo-img"/>
+            Any Buy
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav col-9 flex-row">
+                <div class="nav-item">
+                    <a class="nav-link waves-effect waves-light" accesskey="f"
+                       href="{{action('ProductController@getProduct')}}">商品瀏覽</a>
                 </div>
-            </li>
-            <form class="form-inline">
-                <input class="form-control mr-sm-4" type="text" placeholder="Search" aria-label="Search">
-            </form>
-        </ul>
-        <div class="navbar-nav ml-auto">
-            @yield('right-nav')
-        </div>
 
+                <form class="form-inline" style="flex: 1">
+                    <input class="form-control mr-sm-4" type="search" placeholder="Search" aria-label="Search" style="width: 100%">
+                </form>
+            </div>
+            <div class="navbar-nav ml-auto">
+                @yield('right-nav')
+            </div>
+
+        </div>
     </div>
+
 </nav>
