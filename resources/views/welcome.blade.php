5ceb9734d153b41910984eea5d8240f5458f3449
WeiChun <weichun@csie.tw>
2017-11-29 17:39:18 +0800

    修正字體顏色和card大小

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
             <div class="row">
                 <?php $i = 0;$colNum = 3;?>
                 @foreach($products as $p)
-                    <div class="card-wrap">
+                    <div class="card-wrap col-4">
                         <!--Card-->
                         <a class="card" href="{{action('ProductController@getItem', $p->id)}}">
                             <!--Card image-->
@@ -113,8 +113,8 @@
                             <!--Card content-->
                             <div class="card-body">
                                 <div class="product-info">
-                                    <h5 class="title">{{$p->product_name}}</h5>
-                                    <div class="price">
+                                    <h5 class="product-title">{{$p->product_name}}</h5>
+                                    <div class="product-price">
                                         <i class="ntd">NT$</i>
                                         {{$p->price}}
                                     </div>
