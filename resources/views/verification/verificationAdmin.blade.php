ad2511341b61075295f28fccb00b3e45cdf2dea4
Chen-Tai <qwer2003tw@gmail.com>
2017-12-13 15:51:38 +0800

    新增驗證管理樣式。

--- resources/views/verification/verificationAdmin.blade.php
+++ resources/views/verification/verificationAdmin.blade.php
@@ -1,82 +1,60 @@
-@extends('verification.verification')
-@section('verify')
-    <table border="1">
+@extends('management.base')
+@section('content')
+    <!--Table-->
+    <table class="table table-middle">
+
+        <!--Table head-->
+        <thead class="blue-grey lighten-4">
         <tr>
-            　
-            <td>id</td>
-            　
-            <td>user_id</td>
-            <td>name</td>
-            <td>account</td>
-            <td>role</td>
-            　
-            <td>front_picture</td>
-            　
-            <td>back_picture</td>
-            　
-            <td>verify_result</td>
-            　
-            <td>sign_up_time</td>
-            　
-            <td>verification_time</td>
-            <td>description</td>
-            <td>驗證</td>
+            <th>姓名</th>
+            <th>身分</th>
+            <th>身分證正面</th>
+            <th>身分證背面</th>
+            <th>verify_result</th>
+            <th>註冊時間</th>
+            <th>驗證</th>
         </tr>
+        </thead>
+        <!--Table head-->
+
+        <!--Table body-->
+        <tbody>
         @for ($i = 0; $i < count($data); $i++)
             <tr>
-                　
-                <td>{{$data[$i]->id}}</td>
-                　
-                <td>{{$data[$i]->user_id}}</td>
-                　
                 <td>{{$data[$i]->name}}</td>
-                　
-                <td>{{$data[$i]->account}}</td>
-                　
                 <td>{{$data[$i]->role}}</td>
-                　
                 <td>
-                    <button onclick="OpenImage('{{$data[$i]->id}}','front')">圖片</button>
+                    <a href="{{action('VerificationController@getImage', ['vid'=>$data[$i]->id, 't'=>'front'])}}" class="btn btn-primary btn-sm verify-image">圖片</a>
                 </td>
-                　
                 <td>
-                    <button onclick="OpenImage('{{$data[$i]->id}}','back')">圖片</button>
+                    <a href="{{action('VerificationController@getImage', ['vid'=>$data[$i]->id, 't'=>'back'])}}" class="btn btn-primary btn-sm verify-image">圖片</a>
                 </td>
-                　
                 <td>{{$data[$i]->verify_result}}</td>
-                　
                 <td>{{$data[$i]->sign_up_datetime}}</td>
-                　
-                <td>{{$data[$i]->datetime}}</td>
-                <td>{{$data[$i]->description}}</td>
                 <td>
                     @if($data[$i]->verify_result=="未驗證")
-                        <button onclick="Verification('{{$data[$i]->id}}')">驗證</button>
+                        <button onclick="Verification('{{$data[$i]->id}}')" class="btn btn-primary btn-sm">驗證</button>
                     @endif
                 </td>
             </tr>
         @endfor
+        </tbody>
+
+        <!--Table body-->
     </table>
+    <!--Table-->
+
     {{$data->links()}}
 @endsection
 @section('eofScript')
     <script>
-        function OpenImage(id, t) {
-            var path = 'verify-image/' + id + '/' + t;
-            window.open(path, 'Image', config = 'height=500,width=500');
-        }
+        $(".verify-image").on('click', function (e) {
+            window.open(this.href, 'Image', 'height=400,width=600');
+            e.preventDefault();
+        });
 
         function Verification(id) {
-            var result = "未驗證";
-            var reason = "";
-            var ok = confirm("驗證通過?");
-            if (ok) {
-                result = "驗證成功";
-            }
-            else {
-                reason = prompt("理由?");
-                result = "驗證失敗";
-            }
+            let result = "驗證失敗";
             $.post("verification",
                 {
                     id: id,
@@ -89,3 +67,11 @@
         }
     </script>
 @endsection
+var result = "未驗證";
+var reason = "";
+var ok = confirm("驗證通過?");
+if (ok) {
+result = "驗證成功";
+}
+else {
+reason = prompt("理由?");
