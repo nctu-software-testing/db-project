3363581d966dc97d36a34ea471495deebf39f678
WeiChun <weichun@csie.tw>
2017-12-24 01:17:48 +0800

    修改折扣代碼

--- app/Discount.php
+++ app/Discount.php
@@ -34,22 +34,55 @@ class Discount extends Model
         return strtoupper(hash("crc32b", $str));
     }
 
+    private static function sha256(string $str)
+    {
+        return strtoupper(hash("sha256", $str));
+    }
+
+    private const OCCUPY_DATA = [2, 5, 14, 11, 7, 9];
+
     public static function encrypt(int $id): string
     {
-        $id = strval($id);
-        $crc32 = static::crc32($id);
-        $dataBase64 = base64_encode($id);
+        $arr = ['0000', '0000', '0000', '0000'];
+        //From 0 to 0xfffffff
+        $idHex = str_pad(base_convert($id, 10, 36), 6, '0', STR_PAD_LEFT);
+        $i = 0;
+        for ($i = 0, $j = count(static::OCCUPY_DATA); $i < $j; $i++) {
+            $num = static::OCCUPY_DATA[$i];
+            $a = (int)floor($num / 4);
+            $b = $num % 4;
+            $arr[$a][$b] = $idHex[$i];
+        }
+        $checksum = static::sha256(implode('-', $arr));
+        $start = hexdec(substr($checksum, 11, 7));
+        for ($a = 0; $a < 4; $a++) {
+            for ($b = 0; $b < 4; $b++) {
+                $i = $a* 4 + $b;
+                if(in_array($i, static::OCCUPY_DATA)) continue;
+                $arr[$a][$b] = $checksum[($start + ($i) * 5) % 64];
+            }
+        }
 
-        return $crc32 . $dataBase64;
+        return strtoupper(implode('-', $arr));
     }
 
     public static function decrypt(string $str): ?int
     {
-        $crc32Hex = substr($str, 0, 8);
-        $data = substr($str, 8);
-        $b64DecodedData = base64_decode($data);
-        if (static::crc32($b64DecodedData) === $crc32Hex) {
-            return intval($b64DecodedData);
+        $removeUnusedChar = function (string $str): string {
+            return preg_replace('/[^0-9A-Z]/', '', $str);
+        };
+        $str = $removeUnusedChar($str);
+        $str = strtoupper($str);
+        if (strlen($str) === 16) {
+            $idHex = '';
+            foreach (static::OCCUPY_DATA as $i)
+                $idHex .= $str[$i];
+
+            $id = intval(base_convert($idHex, 36, 10));
+            $enc = $removeUnusedChar(static::encrypt($id));
+
+            if ($enc === $str)
+                return $id;
         }
 
         return null;
