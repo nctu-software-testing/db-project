9e6264fdf7bed79fe9f4eb034e919689aff371b8
Weichun <weichun@csie.tw>
2020-06-12 14:16:52 +0800

    Add HW3 classes

---
+++ tests/Feature/HW3/ShoppingCartCACCTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Tests\Feature\HW3;
+
+
+use Tests\TestCore\BaseTestCase;
+
+class ShoppingCartCACCTest extends BaseTestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
