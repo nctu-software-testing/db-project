27f217e52d23d495a653ed147fc03176b00d99b0
bauuuu1021 <bboy010554@icloud.com>
2020-06-14 00:57:29 +0800

    Add test cases for buyProduct and deleteShoppingCart
    **WIP** Modification of array session

--- tests/Feature/HW3/ShoppingCartPCTest.php
+++ tests/Feature/HW3/ShoppingCartPCTest.php
@@ -3,7 +3,9 @@
 
 namespace Tests\Feature\HW3;
 
-
+use App\Order;
+use App\Product;
+use Carbon\Carbon;
 use Tests\TestCore\BaseTestCase;
 
 class ShoppingCartPCTest extends BaseTestCase
@@ -12,6 +14,7 @@ class ShoppingCartPCTest extends BaseTestCase
     protected function setUp()
     {
         parent::setUp();
+        $this->withUser('c');
     }
 
     protected function tearDown()
@@ -19,8 +22,76 @@ class ShoppingCartPCTest extends BaseTestCase
         parent::tearDown();
     }
 
-    public function testDummy()
+    public function testDeleteShoppingCartCase()
     {
+        $response = $this->delete('shopping-cart', []);
+        $response->assertJson([
+            'result' => 'OK',
+            'success' => true,
+        ]);
+    }
+
+    public function testGetShoppingCartCaseN()
+    {
+        $response = $this->get('shopping-cart', []);
         $this->assertTrue(true);
+    }
+
+    public function testBuyProductCase1()
+    {
+        // cannot find product
+        
+        $this->post('buy', [
+            'id' => 0,
+            'amount' => 1,
+        ])->assertJson([
+            'result' => 'WTF',
+            'success' => false,
+        ]);        
+    }
+
+    public function testBuyProductCase2()
+    {
+        // out of stock
+
+        Carbon::setTestNow('2020-06-13 00:00:00');
+        $targetProduct = Product::find(71);
+        $targetProduct->state = Product::STATE_RELEASE;
+        $targetProduct->amount = 0;
+        $targetProduct->save();
+        
+        $this->post('buy', [
+            'id' => $targetProduct->id,
+            'amount' => 1,
+        ])->assertJson([
+            'result' => '沒有庫存了',
+            'success' => false,
+        ]);        
+    }
+
+    public function testBuyProductCase3()
+    {
+        // not array
+
+        Carbon::setTestNow('2020-06-13 00:00:00');
+        $targetProduct = Product::find(71);
+        $targetProduct->state = Product::STATE_RELEASE;
+        $targetProduct->amount = 100;
+        $targetProduct->save();
+        
+        session(['shoppingcart' => 'tE5Tn0tArR@y']);
+
+        $this->post('buy', [
+            'id' => $targetProduct->id,
+            'amount' => 1,
+        ])->assertJson([
+            'result' => 'OK',
+            'success' => true,
+        ]);        
+    }
+
+    public function testChangeAmountCaseN()
+    {
+        
     }
 }
