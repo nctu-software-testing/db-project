68e5e09f13769e92a64cd0e370f13893a5bf4bea
bauuuu1021 <bboy010554@icloud.com>
2020-06-26 20:54:49 +0800

    Rm first line #!

--- auto.sh
+++ auto.sh
@@ -2,6 +2,6 @@
 
 cp db-template.db tests/resources/db-template.db
 php artisan serve --env=ui.test &
-./main.py -v
+python3 main.py -v
 # jobs -l | grep "php artisan serve" | awk '{print $2}' | xargs kill -9
 ps aux | grep "db-project/server.php" | head -1 | awk '{print $2}' | xargs kill -9
