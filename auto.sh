19e92fc0e7d57db8e9b6218b34b0a4874682b076
bauuuu1021 <bboy010554@icloud.com>
2020-06-29 01:02:02 +0800

    Delete unnecessary files

--- auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp db-template.db tests/resources/db-template.db
-php artisan serve --env=ui.test &
-python3 main.py -v
-# jobs -l | grep "php artisan serve" | awk '{print $2}' | xargs kill -9
-ps aux | grep "db-project/server.php" | head -1 | awk '{print $2}' | xargs kill -9
--- sleep.py
+++
@@ -1,4 +0,0 @@
-import time
-for i in range(18):
-	print(i)
-	time.sleep(10)
